bb3258eb7ca6cc886064974a430d1f7e6ed6258e
Algap02 <106978344+Algap02@users.noreply.github.com>
2022-06-16 10:49:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 apt update -y
 apt install -y docker docker-compose git wget
 git clone https://github.com/Algap02/VIR2-Photoblog-2.git
-cd VIR2-Photoblog-2.git
 sed -i 's/localhost/db/g' /php/classes/db.php
+cd VIR2-Photoblog-2
 docker-compose up -d
 docker-compose exec -T websrv docker-php-ext-install mysql
 docker-compose exec -T websrv /etc/init.d/apache2 reload
